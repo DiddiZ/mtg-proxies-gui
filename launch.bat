0de294f2b24da8a005020600808b180590a4222d
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-09-13 20:50:30 +0200

    Update run script.

--- launch.bat
+++ launch.bat
@@ -1,3 +1,25 @@
+@echo off
+
+REM Update
+git pull --ff-only
+IF %ERRORLEVEL% NEQ 0 ( 
+   pause
+   EXIT /B 1
+)
+
+REM  Install pipenv
 python -m pip install --user -U pipenv
+IF %ERRORLEVEL% NEQ 0 ( 
+   pause
+   EXIT /B 1
+)
+
+REM  Install dependencies
 pipenv install --deploy
+IF %ERRORLEVEL% NEQ 0 ( 
+   pause
+   EXIT /B 1
+)
+
 pipenv run python .\gui.py
+pause
